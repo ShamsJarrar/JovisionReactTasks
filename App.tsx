7bbeb9a4244c9d8eb5823ff9017701962170b0bb
Shams <shamsrami2004@gmail.com>
2025-01-31 22:53:12 +0400

    Task 17 commit

--- App.tsx
+++ App.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import Task16 from "./Tasks/Task 16";
+import Task17 from "./Tasks/Task 17";
 
 export default function App() {
   return (
-    <Task16></Task16>
+    //<Task16></Task16>
+    <Task17></Task17>
   );
 }
